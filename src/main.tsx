ddef641945d22bd6d9b0e061455baf3fcb5f4cdb
zara <zara-email@example.com>
2025-12-10 01:27:43 +0500

    Added new components, updated pages and fixed navbar, hero, stats sections

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-// 1. Import BrowserRouter
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
